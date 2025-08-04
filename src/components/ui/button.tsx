ca960a01f7b716d108b07b5b36b56383a1db6a2d
kwasi <affulnana04@gmail.com>
2025-08-04 09:52:09 +0000

    improve the ghost button colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-secondary/80",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-background/90 hover:text-background-foreground",
+        ghost: "hover:bg-secondary/90 hover:text-background-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
