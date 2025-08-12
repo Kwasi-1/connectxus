da35d8d9c926da1273a4968e6bc2c658352e2a9c
kwasi <affulnana04@gmail.com>
2025-08-12 01:08:53 +0000

    reduce the height of the logo on mobile header

--- src/components/layout/MobileHeader.tsx
+++ src/components/layout/MobileHeader.tsx
@@ -33,7 +33,7 @@ export function MobileHeader({ onMenuClick }: MobileHeaderProps) {
           {/* Logo */}
           <div className="flex items-center space-x-2">
             {/* <img src={logo} alt="Campus Vibe Logo" className="h-12 w-auto rounded-lg" /> */}
-            <Logo className="h-12 w-auto rounded-lg" />
+            <Logo className="h-11 w-auto rounded-lg" />
           </div>
 
           {/* Search Button */}
