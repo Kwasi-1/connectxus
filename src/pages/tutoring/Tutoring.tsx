b3b84b7e6889cc271fc218f976ef87ec3ea7c5ec
kwasi <affulnana04@gmail.com>
2025-08-12 01:54:58 +0000

    revert padding changes

--- src/pages/tutoring/Tutoring.tsx
+++ src/pages/tutoring/Tutoring.tsx
@@ -139,7 +139,7 @@ const Tutoring = () => {
 
   return (
     <AppLayout showRightSidebar={false}>
-      <div className="px-5 py-6 md:p-6 space-y-6 custom-fonts">
+      <div className="p-6 space-y-6 custom-fonts">
         {/* Header */}
         <div className="flex justify-between items-start sm:items-center gap-4">
           <div>
