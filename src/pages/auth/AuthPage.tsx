ef3a7f9040711af5ba2a69f7d18e5775eca1e16f
kwasi <affulnana04@gmail.com>
2025-08-19 12:45:40 +0000

    add logo to auth page

--- src/pages/auth/AuthPage.tsx
+++ src/pages/auth/AuthPage.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { SignInForm } from '@/features/auth/SignInForm';
 import { SignUpForm } from '@/features/auth/SignUpForm';
 import { Card, CardContent } from '@/components/ui/card';
-import smallLogo from '@/assets/connect_small_logo.png';
+import Logo from '@/components/shared/Logo';
 
 export const AuthPage: React.FC = () => {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -14,12 +14,10 @@ export const AuthPage: React.FC = () => {
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-background to-muted/20 p-4">
       <div className={`w-full ${isSignUp ? 'max-w-xl' : 'max-w-lg'}`}>
         <div className="text-center mb-8 custom-font">
-          <div className="w-12 h-12 bg-foreground rounded-xl flex items-center justify-center mx-auto mb-4">
+          <div className="hidden w-12 h-12 bg-foreground rounded-xl fle items-center justify-center mx-auto mb-4">
             <span className="text-background font-bold text-lg">CV</span>
           </div>
-          {/* <h1 className="text-2xl font-bold text-foreground mb-2">Campus Vibe</h1>
-          <p className="text-muted-foreground">Connect with your university community</p> */}
-          {/* <img src={smallLogo} alt="Campus Vibe Logo" className="w-auto mx-auto h-16 hidden xl:block" /> */}
+          <Logo className="w-auto mx-auto h-16 block"/>
         </div>
         
         <Card className="border border-border/40 shadow-lg customfonts rounded-none">
@@ -27,7 +25,13 @@ export const AuthPage: React.FC = () => {
             {isSignUp ? (
               <SignUpForm onToggleMode={toggleMode} />
             ) : (
-              <SignInForm onToggleMode={toggleMode} />
+              <div>
+                <div className="space-y-2 text-center">
+                  <h2 className="text-3xl font-bold">Welcome Back</h2>
+                  <p className="text-muted-foreground">Sign in to your Campus Vibe account</p>
+                </div>
+                <SignInForm onToggleMode={toggleMode} />
+              </div>
             )}
           </CardContent>
         </Card>
