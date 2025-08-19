f071cd342d828e9613a63c9f76fbeddff5f00459
kwasi <affulnana04@gmail.com>
2025-08-19 13:22:39 +0000

    add link to home to logo

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -1,12 +1,15 @@
 import logoWhite from '@/assets/connect_small_logo_white.png'
 import logoblack from '@/assets/connect_small_logo.png'
 import { useTheme } from '@/lib/theme'
+import { Link } from 'react-router-dom'
 
 function Logo({className}) {
   const { theme } = useTheme()
   return (
     <div>
-      <img src={theme === 'dark' ? logoWhite : logoblack} alt="Logo" className={className} />
+      <Link to="/">
+        <img src={theme === 'dark' ? logoWhite : logoblack} alt="Logo" className={className} />
+      </Link>
     </div>
   )
 }
